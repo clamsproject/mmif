5a962c5ddba41642be1aa73b02c6d259380fd4db
Angus L'Herrou <piraka@brandeis.edu>
2020-06-23 16:10:29 -0400

    Added a bad example and a test for validating bad MMIF

--- mmif-python/tests/mmif_examples.py
+++ mmif-python/tests/mmif_examples.py
@@ -43,3 +43,49 @@ example1 = """{
     }
   ]
 }"""
+
+example2 = """{
+  "metadata": { },
+  "contains": "???",
+  "media": [
+    {
+      "id": "m1",
+      "type": "image",
+      "mime": "image/jpeg",
+      "location": "/var/archive/image-0012.jpg"
+    },
+    {
+      "id": "m2",
+      "type": "text",
+      "text": {
+        "@value": "yelp",
+        "@language": "en"
+      },
+      "metadata": {
+        "source": "v1:bb1",
+        "tool": "http://tools.clams.io/tesseract/1.2.1"
+      }
+    }
+  ],
+  "views": [
+    {
+      "id": "v1",
+      "metadata": {
+        "contains": {
+          "BoundingBox": {"unit": "pixels"}
+        },
+        "medium": "m1",
+        "tool": "http://tools.clams.io/east/1.0.4"
+      },
+      "annotations": [
+        {
+          "@type": "BoundingBox",
+          "id": "bb1",
+          "properties": {
+            "coordinates": [[90,40], [110,40], [90,50], [110,50]] }
+        }
+      ]
+    }
+  ]
+}"""
+
--- mmif-python/tests/test_serialize.py
+++ mmif-python/tests/test_serialize.py
@@ -1,13 +1,26 @@
 import unittest
 import json
+import traceback
 import mmif
-from tests.mmif_examples import example1
+from tests.mmif_examples import *
 
 
 class TestMmif(unittest.TestCase):
     def test_mmif_deserialize(self):
-        mmif_obj = mmif.serialize.Mmif(example1)
+        try:
+            mmif_obj = mmif.serialize.Mmif(example1)
+        except AssertionError:
+            traceback.print_exc()
+            self.fail()
         self.assertEqual(json.loads(example1), mmif_obj.serialize())
+
+    def test_bad_mmif_deserialize(self):
+        try:
+            mmif_obj = mmif.serialize.Mmif(example2)
+            self.fail()
+        except AssertionError:
+            traceback.print_exc()
+
 
 
 if __name__ == '__main__':
