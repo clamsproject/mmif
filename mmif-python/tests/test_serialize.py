5096e18ae96bba337c7848869170a7f49bc0bb41
Angus L'Herrou <piraka@brandeis.edu>
2020-06-23 16:38:22 -0400

    Changed test logic for creating bad serialized data; added some more basic tests

--- mmif-python/tests/test_serialize.py
+++ mmif-python/tests/test_serialize.py
@@ -1,26 +1,70 @@
 import unittest
 import json
+import sys
 import traceback
 import mmif
 from tests.mmif_examples import *
 
 
 class TestMmif(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.mmif_json: dict = json.loads(example1)
+
     def test_mmif_deserialize(self):
+        json_str = json.dumps(self.mmif_json)
         try:
-            mmif_obj = mmif.serialize.Mmif(example1)
+            mmif_obj = mmif.serialize.Mmif(json_str)
         except AssertionError:
+            print("Assertion failed on: ")
             traceback.print_exc()
             self.fail()
-        self.assertEqual(json.loads(example1), mmif_obj.serialize())
+        self.assertEqual(self.mmif_json, mmif_obj.serialize())
 
-    def test_bad_mmif_deserialize(self):
+    def test_bad_mmif_deserialize_no_context(self):
+        self.mmif_json.pop('@context')
+        json_str = json.dumps(self.mmif_json)
         try:
-            mmif_obj = mmif.serialize.Mmif(example2)
+            mmif_obj = mmif.serialize.Mmif(json_str)
             self.fail()
         except AssertionError:
-            traceback.print_exc()
+            pass
 
+    def test_bad_mmif_deserialize_no_metadata(self):
+        self.mmif_json.pop('metadata')
+        json_str = json.dumps(self.mmif_json)
+        try:
+            mmif_obj = mmif.serialize.Mmif(json_str)
+            self.fail()
+        except AssertionError:
+            pass
+
+    def test_bad_mmif_deserialize_no_media(self):
+        self.mmif_json.pop('media')
+        json_str = json.dumps(self.mmif_json)
+        try:
+            mmif_obj = mmif.serialize.Mmif(json_str)
+            self.fail()
+        except AssertionError:
+            pass
+
+    def test_bad_mmif_deserialize_no_views(self):
+        self.mmif_json.pop('views')
+        json_str = json.dumps(self.mmif_json)
+        try:
+            mmif_obj = mmif.serialize.Mmif(json_str)
+            self.fail()
+        except AssertionError:
+            pass
+
+    def test_bad_mmif_deserialize_no_contains(self):
+        self.mmif_json.pop('contains')
+        json_str = json.dumps(self.mmif_json)
+        try:
+            mmif_obj = mmif.serialize.Mmif(json_str)
+            self.fail()
+        except AssertionError:
+            pass
 
 
 if __name__ == '__main__':
