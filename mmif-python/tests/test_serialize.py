796f8c5469a1c50ae429eaadaa69a3442a81972f
Angus L'Herrou <piraka@brandeis.edu>
2020-06-23 16:03:18 -0400

    Added an example MMIF JSON string to use with unit tests

--- mmif-python/tests/test_serialize.py
+++ mmif-python/tests/test_serialize.py
@@ -1,15 +1,13 @@
 import unittest
-import os
 import json
 import mmif
+from tests.mmif_examples import example1
 
 
 class TestMmif(unittest.TestCase):
     def test_mmif_deserialize(self):
-        with open(os.path.join('..', 'mmif', 'mmif_example_for_reference.json'), 'r') as json_file:
-            json_str = json_file.read()
-            mmif_obj = mmif.serialize.Mmif(json_str)
-            self.assertEqual(json.loads(json_str), mmif_obj.serialize())
+        mmif_obj = mmif.serialize.Mmif(example1)
+        self.assertEqual(json.loads(example1), mmif_obj.serialize())
 
 
 if __name__ == '__main__':
