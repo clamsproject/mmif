31d3b084f888da72eb71eddc8f1426d019d25651
Angus L'Herrou <piraka@brandeis.edu>
2020-07-23 18:22:46 -0400

    added tests for schema and view; fixed a couple bugs

--- mmif-python/tests/test_serialize.py
+++ mmif-python/tests/test_serialize.py
@@ -1,9 +1,15 @@
 import unittest
 import json
 
+import mmif
+from hypothesis import given, strategies as st, settings, HealthCheck
+import hypothesis_jsonschema
+
 import pytest
 from jsonschema import ValidationError
-from mmif.serialize import Mmif
+from mmif.serialize import Mmif, Annotation, View
+from pkg_resources import resource_stream
+
 from tests.mmif_examples import *
 
 
@@ -65,6 +71,70 @@ class TestMmif(unittest.TestCase):
             Mmif(mmif.serialize())
             assert "validating 'oneOf'" in ve.value
 
+    def test___getitem__(self):
+        pass
+
+
+class TestView(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.view_json = json.loads(view1)
+        self.view_obj = View(view1)
+        self.maxDiff = None
+
+    def test_init(self):
+        try:
+            _ = View(view1)
+        except Exception as ex:
+            self.fail(ex.message)
+
+    def test_annotation_order_preserved(self):
+        view_serial = self.view_obj.serialize()
+        for original, new in zip(self.view_json['annotations'],
+                                 json.loads(view_serial)['annotations']):
+            assert original['properties']['id'] == new['properties']['id'], \
+                f"{original['properties']['id']} is not {new['properties']['id']}"
+
+    def test_props_preserved(self):
+        view_serial = self.view_obj.serialize()
+
+        def id_func(a):
+            return a['properties']['id']
+
+        for original, new in zip(sorted(self.view_json['annotations'], key=id_func),
+                                 sorted(json.loads(view_serial)['annotations'], key=id_func)):
+            assert original == new
+
+    def test___getitem__(self):
+        pass
+
+
+class TestSchema(unittest.TestCase):
+
+    schema_res = resource_stream(f'{mmif.__name__}.{mmif._res_pkg}', mmif._schema_res_name)
+    schema = json.load(schema_res)
+    schema_res.close()
+
+    def setUp(self) -> None:
+        if DEBUG:
+            self.hypos = []
+
+    def tearDown(self) -> None:
+        if DEBUG:
+            with open('hypotheses.json', 'w') as dump:
+                json.dump(self.hypos, dump, indent=2)
+
+    @given(hypothesis_jsonschema.from_schema(schema))
+    @settings(suppress_health_check=HealthCheck.all())
+    def test_accepts_valid_schema(self, data):
+        if DEBUG:
+            self.hypos.append(data)
+        try:
+            _ = Mmif(json.dumps(data))
+        except ValidationError as ve:
+            self.fail("didn't accept valid data")
+
 
 if __name__ == '__main__':
+    DEBUG = False
     unittest.main()
