f8cc50327772ac09d6e9fb37d1b1105c79bb5baf
Keigh Rim <keigh.rim@gmail.com>
2020-11-29 19:23:16 -0500

    deleted python SDK files, see #112

--- mmif-python/mmif/serialize/__init__.py
+++
@@ -1,8 +0,0 @@
-from .annotation import __all__ as anno_all
-from .annotation import *
-from .mmif import __all__ as mmif_all
-from .mmif import *
-from .view import __all__ as view_all
-from .view import *
-
-__all__ = anno_all + mmif_all + view_all
